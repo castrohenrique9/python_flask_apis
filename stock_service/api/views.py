05a8626e27a953643891813fed0f1103c08b12fd
Castro Souza <castrohenrique9@gmail.com>
2022-07-07 19:46:53 -0300

    add error handler for GenericException and DataNotFoundException

--- stock_service/api/views.py
+++ stock_service/api/views.py
@@ -7,6 +7,8 @@ from stock_service.api.resources import StockResource
 
 from urllib.error import URLError
 
+from stock_service.exceptions import DataNotFoundException, GenericException
+
 
 blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 api = Api(blueprint)
@@ -17,12 +19,14 @@ api.add_resource(StockResource, "/stock/<string:stock_code>", endpoint="stock")
 
 @blueprint.errorhandler(ValidationError)
 def handle_marshmallow_error(e):
-    return jsonify(e.messages), 400
+    return jsonify(e.message), 400
 
 @blueprint.errorhandler(URLError)
 @blueprint.errorhandler(AttributeError)
+@blueprint.errorhandler(DataNotFoundException)
+@blueprint.errorhandler(GenericException)
 def handle_error_404(e):
-    return jsonify(e.messages), 404
+    return jsonify(e.message), 404
 
 @blueprint.errorhandler(Exception)
 def handle_error_500():
