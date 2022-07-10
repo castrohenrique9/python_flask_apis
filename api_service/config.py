6afd8917fc06b9c400237513bf615c659b0a806b
Castro Souza <castrohenrique9@gmail.com>
2022-07-10 12:27:44 -0300

    WIP: setting rabbit api_service

--- api_service/config.py
+++ api_service/config.py
@@ -13,3 +13,12 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 URL_EXTERNAL_STOCK = os.getenv("URL_EXTERNAL_STOCK")
 QUERY_ROW_LIMIT_DEFAULT = os.getenv("QUERY_ROW_LIMIT_DEFAULT", default=5)
+
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", default="127.0.0.1")
+RABBITMQ_PORT = os.getenv("RABBITMQ_PORT", default="5672")
+RABBITMQ_VHOST = os.getenv("RABBITMQ_VHOST", default="/")
+RABBITMQ_USER = os.getenv("RABBITMQ_USER", default="guest")
+RABBITMQ_PWD = os.getenv("RABBITMQ_PWD", default="guest")
+RABBITMQ_EXCHANGE = os.getenv("RABBITMQ_EXCHANGE", default="jobsity")
+RABBITMQ_EXCHANGE_TYPE = os.getenv("RABBITMQ_EXCHANGE_TYPE", default="direct")
+RABBITMQ_QUEUE_STOCK = os.getenv("RABBITMQ_QUEUE_STOCK", default="stock_service")
