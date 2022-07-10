192f71e9637ca8cc7d8466cb016206547a2711d6
Castro Souza <castrohenrique9@gmail.com>
2022-07-10 12:32:59 -0300

    WIP: setting rabbit stock_service

--- stock_service/config.py
+++ stock_service/config.py
@@ -8,3 +8,12 @@ ENV = os.getenv("FLASK_ENV")
 DEBUG = ENV == "development"
 SECRET_KEY = os.getenv("SECRET_KEY")
 URL_EXTERNAL_STOCK = os.getenv("URL_EXTERNAL_STOCK")
+
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", default="127.0.0.1")
+RABBITMQ_PORT = os.getenv("RABBITMQ_PORT", default="5672")
+RABBITMQ_VHOST = os.getenv("RABBITMQ_VHOST", default="/")
+RABBITMQ_USER = os.getenv("RABBITMQ_USER", default="guest")
+RABBITMQ_PWD = os.getenv("RABBITMQ_PWD", default="guest")
+RABBITMQ_EXCHANGE = os.getenv("RABBITMQ_EXCHANGE", default="jobsity")
+RABBITMQ_EXCHANGE_TYPE = os.getenv("RABBITMQ_EXCHANGE_TYPE", default="direct")
+RABBITMQ_QUEUE_STOCK = os.getenv("RABBITMQ_QUEUE_STOCK", default="stock_service")
--- stock_service/extensions.py
+++ stock_service/extensions.py
@@ -1,5 +1,19 @@
 # encoding: utf-8
 
 from flask_marshmallow import Marshmallow
+import pika
+from api_service.config import RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_VHOST, RABBITMQ_EXCHANGE, RABBITMQ_EXCHANGE_TYPE, RABBITMQ_QUEUE_STOCK
 
 marsh = Marshmallow()
+
+rabbitmq_connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT, virtual_host=RABBITMQ_VHOST))
+rabbitmq_channel = rabbitmq_connection.channel()
+rabbitmq_channel.exchange_declare(exchange=RABBITMQ_EXCHANGE, exchange_type=RABBITMQ_EXCHANGE_TYPE)
+rabbitmq_channel.queue_declare(queue=RABBITMQ_QUEUE_STOCK)
+rabbitmq_channel.queue_bind(exchange=RABBITMQ_EXCHANGE, queue=RABBITMQ_QUEUE_STOCK, routing_key="tag_stock")
+
+def callback(ch, method, properties, body):
+    print(body)
+
+rabbitmq_channel.basic_consume(queue=RABBITMQ_QUEUE_STOCK, on_message_callback=callback, auto_ack=True)
+rabbitmq_channel.start_consuming()
