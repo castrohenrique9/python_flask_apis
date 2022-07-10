fe5bb5b05c5f073c9aa46b70491140d82ba7ece6
Castro Souza <castrohenrique9@gmail.com>
2022-07-10 14:20:42 -0300

    WIP: reading get from queue in stock_service

--- stock_service/app.py
+++ stock_service/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from stock_service import api
 
+from stock_service.extensions import rabbitmq_connection
+from stock_service.config import RABBITMQ_EXCHANGE, RABBITMQ_EXCHANGE_TYPE, RABBITMQ_QUEUE_STOCK
+
+rabbitmq_channel = None
 
 def create_app(testing=False):
     app = Flask("stock_service")
@@ -10,6 +14,7 @@ def create_app(testing=False):
         app.config["TESTING"] = True
 
     register_blueprints(app)
+    create_rabbitmq_channel()
 
     return app
 
@@ -17,7 +22,23 @@ def create_app(testing=False):
 def register_blueprints(app):
     app.register_blueprint(api.views.blueprint)
 
+def callback_test(ch, method, properties, body):
+    print("liiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii 2222")
+    print(body)
+
+def create_rabbitmq_channel():
+    rabbitmq_channel = rabbitmq_connection.channel()
+    rabbitmq_channel.exchange_declare(exchange=RABBITMQ_EXCHANGE, exchange_type=RABBITMQ_EXCHANGE_TYPE)
+    rabbitmq_channel.queue_declare(queue=RABBITMQ_QUEUE_STOCK)
+    rabbitmq_channel.queue_bind(exchange=RABBITMQ_EXCHANGE, queue=RABBITMQ_QUEUE_STOCK, routing_key="tag_stock")
+    
+    rabbitmq_channel.basic_consume(queue=RABBITMQ_QUEUE_STOCK, on_message_callback=callback_test, auto_ack=True)
+    rabbitmq_channel.start_consuming()
+    return rabbitmq_channel
 
 if __name__ == "__main__":
     app = create_app(False)
+    rabbitmq_channel = create_rabbitmq_channel()
+    rabbitmq_channel.start_consuming()
+
     app.run(host="0.0.0.0", port=5001)
--- stock_service/wsgi.py
+++ stock_service/wsgi.py
@@ -1,3 +1,6 @@
-from stock_service.app import create_app
+from stock_service.app import create_app, create_rabbitmq_channel
 
 app = create_app()
+
+rabbitmq_channel = create_rabbitmq_channel()
+rabbitmq_channel.start_consuming()
